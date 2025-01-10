1096416b97f753944817a5bc3e724417de244ac9
Kabelo Tlhape <kabelo.tlhape@ilabqa.com>
2025-01-10 15:10:36 +0200

    Delete src directory

--- src/StudentGrades.java
+++
@@ -1,9 +0,0 @@
-public class StudentGrades {
-
-    // TODO: define the 'getGreetings()' method
-
-    // TODO: define the 'determineGrade()' method
-
-    // TODO: define the 'determineAge()' method
-
-}
